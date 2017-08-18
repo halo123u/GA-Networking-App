a44bfd4563018d18daf88da70200b51ee862518b
Amidou Kante <kamdiou@gmail.com>
2017-08-18 15:45:24 -0400

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-import logo from './logo.svg';
+import firebaseui from 'firebaseui';
+import User from './components/User';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
@@ -18,19 +21,51 @@ class App extends Component {
       storageBucket: "ga-tinder.appspot.com",
       messagingSenderId: "511990920076"
     };
+
     firebase.initializeApp(config);
+    var uiConfig = {
+        signInSuccessUrl: '/user',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: null
+      };
+
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
+
   }
+
+
   render() {
+    
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+    <Router>
+      {/* <Switch> */}
+        <div className="App">
+          <div className="App-header">
+            <h1>Welcome to My Awesome App</h1>
+          </div>
+          <div id="firebaseui-auth-container"></div>
+          <div className="authStatus">
+              <div id="sign-in-status"></div>
+              <div id="sign-in"></div>
+              <div id="account-details"></div>
+          </div>
+          <Route exact path='' component={this.App} />
+          <Route exact path='/user' component={User} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      {/* </Switch> */}
+    </Router>
     );
   }
 }
