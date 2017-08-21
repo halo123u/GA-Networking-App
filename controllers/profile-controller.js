68f755cb0a54f306a9c2d22c97cb54be3e0977f5
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-08-21 16:47:48 -0400

    add controller for CreateProfile

--- controllers/profile-controller.js
+++ controllers/profile-controller.js
@@ -3,8 +3,27 @@ const Profile = require('../models/profile');
 const profileController = {
     getProfile: (req,res)=>{
         Profile.getInfo(req.user.id).then(data=>{
+            console.log(data);
+            console.log('hello');
             res.json(data);
-        })
+        });
+    },
+    createProfile: (req,res) => {
+        Profile.setInfo({
+            age: req.body.age,
+            class: req.body.class,
+            cohort: req.body.cohort,
+            interest : req.body.interest,
+            location : req.body.location,
+            bio : req.body.bio,
+            pic : req.body.picture_url
+        }).then(data=>{
+                console.log(data);
+                res.send(data);
+            }).catch(err=>{
+                console.log(err);
+                res.send(err);
+            });
     }
 }
 
