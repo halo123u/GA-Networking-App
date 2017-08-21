d06d6291b431457fbcc6b33c767a864042b68053
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-08-21 18:14:59 -0400

    add route for the GET profile/:id

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const profile = express.Router();
 const profileController = require('../controllers/profile-controller');
 
-profile.get('/',profileController.getProfile);
+profile.get('/:id',profileController.getProfile);
 profile.post('/',profileController.createProfile);
 
 module.exports =  profile;
