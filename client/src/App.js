3f35918f7ee5eed6387df4733228de1c41ea9d4c
Amidou Kante <kamdiou@gmail.com>
2017-08-21 15:19:20 -0400

    basic home layout

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
+import Feed from './components/Feed';
+import Message from './components/Message';
 import Profile from './components/Profile';
 import Footer from './components/Footer';
 
@@ -20,9 +22,11 @@ class App extends Component {
           <Header />
           <div className="container">
             <Route exact path="/" component={Home} />
-            {/* <Route exact path="/Login" component={Login} />
-            <Route exact path="/Register" component={Register} /> */}
-            <Route exact path="/Profile" component={Profile} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/feed" component={Feed} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/messages" component={Message} />
           </div>
           <Footer />
         </div>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 
-class Header extends Component {
-    render(){
+const Header = () => {
     return(
         <header>
             <nav>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/login'>Login</Link></li>
                 <li><Link to='/register'>Register</Link></li>
+                <li><Link to='/feed'>Discover</Link></li>
                 <li><Link to='/profile'>Profile</Link></li>
+                <li><Link to='/messages'>DMs</Link></li>
             </nav>
         </header>
     )
-    }
 }
 
 export default Header;
