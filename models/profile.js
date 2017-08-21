0b8a50bd1fb9ae9a0845c474a3c1930bf9d98a61
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-08-21 18:13:04 -0400

    add setInfo on the Profile model

--- models/profile.js
+++ models/profile.js
@@ -1,22 +1,17 @@
 const db = require('../db/config');
 
-Profile ={
-   getInfo: () =>{
-       return db.query(`SELECT * FROM profile`);
+
+const Profile ={
+   getInfo: (id) =>{
+       return db.query(`SELECT * FROM profile
+        WHERE user_id=$1`, [id]);
     },
     setInfo: (profile)=>{
         return db.one(`
-        INSERT INTO (age,class,cohort,interest,location,bio,picture_url)
-        age INTEGER,
-        class VARCHAR(255),
-        cohort VARCHAR(255),
-        interest VARCHAR(255),
-        location VARCHAR(50),
-        bio VARCHAR(140),
-        picture_url VARCHAR(255),
-        VALUES($1,$2,$3,$4,$5,$6,$7)
-        RETURNING *`
-        [profile.age,profile.class,profile.cohort,profile.interest, profile.location,profile.bio,profile.picture_url])
+        INSERT INTO profile(age,class,cohort,interest,location,bio,picture_url, user_id)
+        VALUES($1,$2,$3,$4,$5,$6,$7,$8)
+        RETURNING *`,
+        [profile.age,profile.class,profile.cohort,profile.interest, profile.location,profile.bio,profile.picture_url,profile.user_id])
     }
 
 }
