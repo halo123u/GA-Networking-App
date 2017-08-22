423f45510398555685e3de074bcc57a236c5ba24
Karen Baque <k.baque@aol.com>
2017-08-22 15:41:07 -0400

    add edit-form

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -5,9 +5,30 @@ class Feed extends Component{
     constructor(props){
         super(props);
         this.state = {
-
+            age: '',
+            class: '',
+            cohort: '',
+            interest: '',
+            location: '',
+            bio: '',
+            picture_url: ''
         }
     }
+
+    
+    handleEventChange(e) {
+        this.setState ({
+            age: '',
+            class: '',
+            cohort: '',
+            interest: '',
+            location: '',
+            bio: '',
+            picture_url: ''
+
+        })
+    }
+
 
     render(){
         return(
