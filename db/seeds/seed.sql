aa8fa9484f1437009254854b64bc912f0d56b672
Amidou Kante <kamdiou@gmail.com>
2017-08-22 09:31:44 -0400

    update

--- db/seeds/seed.sql
+++ db/seeds/seed.sql
@@ -4,11 +4,14 @@ INSERT INTO users
 VALUES
 ('halo4', 'halo4', 'something4','oswaldo', 'almazo'),
 ('halo1', 'halo1', 'something1','oswaldo2', 'almazo2'),
+('halo2', 'halo2', 'something2','oswaldo3', 'almazo3'),
+('halo2', 'halo2', 'something2','oswaldo3', 'almazo3'),
 ('halo2', 'halo2', 'something2','oswaldo3', 'almazo3');
 
 INSERT INTO profile
 (id, age, class, cohort,  interest, location, bio, user_id)
 VALUES
-(1,28, 'WDI', 'Delorean', 'Back-end development', 'New York City', 'I have a computer science degree from X university and enrolled in WDI to sharpen my skills. Looking fo',5),
-(2,24, 'UXDI', 'twin peaks', 'client interaction', 'New York City', 'Looking for web developer to help make a networking app. I have extensive design background special',4),
-(3,30, 'IOS Dev','some name', 'Games', 'New York City', 'Want to make a sidescroll shooter game. Need UX designer to help figure out layout of elements on the screen.',3)
+(1,28, 'WDI', 'Delorean', 'Back-end development', 'New York City', 'I have a computer science degree from X university and enrolled in WDI to sharpen my skills. Looking fo',1),
+(2,24, 'UXDI', 'twin peaks', 'client interaction', 'New York City', 'Looking for web developer to help make a networking app. I have extensive design background special',2),
+(3,30, 'IOS Dev','some name', 'Games', 'New York City', 'Want to make a sidescroll shooter game. Need UX designer to help figure out layout of elements on the screen.',3
+)
