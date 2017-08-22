5dd4bed18d44661e0a0806d69229efddfc5c5814
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-22 16:24:47 -0400

    allow app to retrieve messages by sender and receiver

--- routes/message-routes.js
+++ routes/message-routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const messages = express.Router();
 const messageController = require('../controllers/message-controller');
 
-messages.get('/', messageController.getAllMessages);
+messages.get('/sent/:id', messageController.getAllSentMessages);
+messages.get('/received/:id', messageController.getAllReceivedMessages);
 messages.get('/:id', messageController.getMessageById);
-messages.post('/', messageController.createMessage);
+messages.post('/:user_id', messageController.createMessage);
 messages.delete('/:id', messageController.deleteMessage);
 
 module.exports = messages;
