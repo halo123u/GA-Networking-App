be17cf2e8b5311af04852e46fa77cf3af644e23e
Amidou Kante <kamdiou@gmail.com>
2017-08-23 13:47:47 -0400

    redirect on profile page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
 
@@ -6,24 +7,43 @@ class Profile extends Component{
     constructor(props){
         super(props);
         this.state = {
-            profileInfo: null
-
+            profileInfo: null,
+            redirect: false
         }
     }
     componentDidMount() {
         console.log('Fetching profile')
-        if(this.props.data !== undefined){
+        if(this.props.data !== null){
             console.log(this.props.data.id)
             axios.get(`/profile/${this.props.data.id}`)
             .then((res) => {
                 console.log(res.data[0])
                 this.setState ({
-                    profileInfo: res.data[0]
+                    profileInfo: res.data[0],
+                    redirect: false
                 })
             }).catch(err => console.log(err))
             console.log(this.props.data)
         } else {
             console.log('Profile not loaded')
+            this.setState({
+                redirect: true
+            })
+        }
+    }
+
+    componentWillMount() {
+        console.log('Checking Logged in Status')
+        if(this.props.authState){
+            console.log('logged in already')
+            this.setState({
+                redirect: false
+            })
+        }else{
+            console.log('not logged in')
+            this.setState({
+                redirect: true
+            })
         }
     }
 
@@ -41,13 +61,15 @@ class Profile extends Component{
           </div>
          )
         } else {
-            return <p> className="loading">Loading...</p>
+            return <p className="loading">Loading...</p>
         }
     }
     
     render(){
+        const {redirect} = this.state;
         return(
             <div className="Profile">
+                {redirect ?(<Redirect to='/login'/>) : null}
                 <h1>Profile</h1>
             <ul>
             <li> {this.renderProfile()} </li> 
