d8212b3dde39710faad64e61778d467b61b24aaa
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-23 11:19:39 -0400

    can render multiple user profiles

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -6,31 +6,42 @@ class Feed extends Component{
     constructor(props){
         super(props);
         this.state = {
-            first_name: '',
-            last_name: '',
-            age: '',
-            class: '',
-            // cohort: '',
-            // interest: '',
-            // location: '',
-            bio: '',
-        //     picture_url: ''
+            data: null,
+            dataLoaded: false,
         }
+      this.renderFeed = this.renderFeed.bind(this);  
     }
 
     componentDidMount() {
-        axios.get('/profile/feed').then(res => {
-           console.log(res);
-           console.log(res.data);
-           console.log(res.data[0]);
-            this.setState({
-                first_name: res.data[0].first_name,
-                last_name: res.data[0].last_name,
-                age: res.data[0].age,
-                class: res.data[0].class,
-                bio: res.data[0].bio,
-           }) 
-        })
+        this.setState({userInfo: this.props.data})
+        if (this.state.userInfo !== null) {
+            axios.get('/profile/feed').then(res => {
+                this.setState({
+                    data: res.data,
+                    dataLoaded: true,
+                })
+            }).catch(err=> {console.log(err)});
+        }
+    }
+
+    renderFeed() {
+        console.log(this.state.data);
+        if (this.state.dataLoaded) {
+            return (
+              <div className='feed-container'>  
+                {this.state.data.map(profile => {
+                    return (
+                        <div className='feed-profile' key={}>
+                            <h2>{profile.first_name} {profile.last_name}</h2>
+                            <h3>{profile.age}</h3>
+                            <h3>{profile.class}</h3>
+                            <p>{profile.bio}</p>
+                        </div>    
+                    )
+                })}
+            </div>
+            )
+        }
     }
     
     // handleEventChange(e) {
@@ -51,10 +62,7 @@ class Feed extends Component{
         return(
            <div className='feed-container'> 
             <h1>Feed</h1>
-            <h2>{this.state.first_name} {this.state.last_name}</h2>
-            <h3>{this.state.age}</h3>
-            <h3>{this.state.class}</h3>
-            <p>{this.state.bio}</p>
+            {this.renderFeed()}
            </div> 
         )
     }
