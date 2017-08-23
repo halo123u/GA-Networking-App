f80a8357b050b8941db1d6a464506b59ecf358d4
Karen Baque <k.baque@aol.com>
2017-08-23 10:19:49 -0400

    add profile info

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Profile extends Component{
     constructor(props){
         super(props);
         this.state = {
             profileInfo: null
+
         }
     }
     componentDidMount() {
-        // axios.get(`profile/${}`)
+        if(this.props.data !== undefined){
+            console.log(this.props.data)
+            this.setState ({
+                profileInfo: this.props.data
+            })
+        } else {
+            console.log('Profile not loaded')
+        }
     }
+    
     render(){
         return(
             <div className="Profile">
                 <h1>Profile</h1>
+            <ul>
+              <li><p>{this.state.profileInfo.picture_url}</p></li>
+              <li><p>{this.state.profileInfo.bio}</p></li>
+              <li><p>{this.state.profileInfo.age} </p></li> 
+              <li><p>{this.state.profileInfo.class_name}</p></li> 
+              <li><p>{this.state.profileInfo.cohort}</p></li>
+              <li><p>{this.state.profileInfo.interest}</p></li>
+              <li><p>{this.state.profileInfo.location}</p></li>
+            </ul> 
             </div>
         )
     }
