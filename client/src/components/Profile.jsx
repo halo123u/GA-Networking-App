79e6aab49da1f1461856cf44d334e52f9201ed22
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-23 15:41:12 -0400

    prevent duplication of renderProfile function

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -46,30 +46,12 @@ class Profile extends Component{
          return(
           <div className="profile-info">
             <li><p><img src={this.state.profileInfo.picture_url} /></p> </li>
-            <li><p>{this.state.profileInfo.bio} </p></li>
-            <li><p>{this.state.profileInfo.age} </p></li>
-            <li><p>{this.state.profileInfo.class}</p></li>
-            <li><p>{this.state.profileInfo.cohort} </p></li>
-            <li><p>{this.state.profileInfo.interest} </p></li>
-            <li><p>{this.state.profileInfo.location} </p></li>
-          </div>
-         )
-        } else {
-            return <p className="loading">Loading...</p>
-        }
-    }
-
-    renderProfile(){
-        if(this.state.profileInfo) {
-         return(
-          <div className="profile-info">
-            <li><p><img src={this.state.profileInfo.picture_url} /></p> </li>
-            <li><p>{this.state.profileInfo.bio} </p></li>
-            <li><p>{this.state.profileInfo.age} </p></li>
-            <li><p>{this.state.profileInfo.class}</p></li>
-            <li><p>{this.state.profileInfo.cohort} </p></li>
-            <li><p>{this.state.profileInfo.interest} </p></li>
-            <li><p>{this.state.profileInfo.location} </p></li>
+            <li>Bio:<p>{this.state.profileInfo.bio} </p></li>
+            <li>Age: <p>{this.state.profileInfo.age} </p></li>
+            <li>Class: <p>{this.state.profileInfo.class}</p></li>
+            <li>Cohort: <p>{this.state.profileInfo.cohort} </p></li>
+            <li>Interest: <p>{this.state.profileInfo.interest} </p></li>
+            <li>Location: <p>{this.state.profileInfo.location} </p></li>
           </div>
          )
         } else {
