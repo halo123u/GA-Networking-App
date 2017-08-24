145271374bbf7a1e5aa4af2492bb43da084e05dd
Amidou Kante <kamdiou@gmail.com>
2017-08-24 09:32:57 -0400

    css for home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -12,6 +12,25 @@ class Home extends Component{
     
     render(){
         return(
+            <div class="demo-card-wide mdl-card mdl-shadow--2dp">
+                <div class="mdl-card__title">
+                    <h2 class="mdl-card__title-text">GA Networking App</h2>
+                </div>
+                <div class="mdl-card__supporting-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Mauris sagittis pellentesque lacus eleifend lacinia...
+                </div>
+                <div class="mdl-card__actions mdl-card--border">
+                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                    Get Started
+                    </a>
+                </div>
+                <div class="mdl-card__menu">
+                    <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+                    <i class="material-icons">share</i>
+                    </button>
+                </div>
+            </div>
             <div className="appHome">
                 <h1>
                     GA Tinder
