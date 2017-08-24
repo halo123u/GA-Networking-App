4f51320a6227638ca001fced54c1d403fecca20f
Amidou Kante <kamdiou@gmail.com>
2017-08-24 15:04:43 -0400

    minor changes to the login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -23,18 +23,26 @@ class Login extends Component{
     render(){
 
         return(
-            <div className="login-page">
+            <div className="login-page mdl-card mdl-shadow--8dp">
+                <div className="mdl-card__title">
+                    <h2 className="mdl-card__title-text">Login</h2>
+                </div>
                 <form onSubmit={(e) => this.props.submit(e, this.state.username, this.state.password)}>
-                  <div className="mdl-textfield mdl-js-textfield">
-              <input className="mdl-textfield__input" id="username" type="text" required="true" name="username" value={this.state.username} placeholder="Username" onChange={this.handleInputChange} />
-              <label className="mdl-textfield__label" for="username">Username...</label>
-              <input className="mdl-textfield__input" id="password" type="password" required="true" name="password" value={this.state.password} placeholder="Password" onChange={this.handleInputChange} />
-              <label className="mdl-textfield__label" for="password">Password...</label>
-              <input className="mdl-textfield__input" id="login" type="submit" value='Log In!' onClick={this.handleSubmit} />
-              <label className="mdl-textfield__label" for="login">Log In!...</label>
+                    <div className="mdl-card__supporting-text">
+                        <div className="mdl-textfield mdl-js-textfield">
+                            <input className="mdl-textfield__input" id="username" type="text" required="true" name="username" value={this.state.username} placeholder="Username" onChange={this.handleInputChange} />
+                            <label className="mdl-textfield__label" htmlFor="username">Username...</label>
+                        </div>
+                        <div className="mdl-textfield mdl-js-textfield">
+                            <input className="mdl-textfield__input" id="password" type="password" required="true" name="password" value={this.state.password} placeholder="Password" onChange={this.handleInputChange} />
+                            <label className="mdl-textfield__label" htmlFor="password">Password...</label>
+                        </div>
+                    </div>
+                    <div className="mdl-card__actions mdl-card--border">
+                        <input className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--primary" type="submit" value='Log In!' onClick={this.handleSubmit} />
+                    </div>
+                </form>
             </div>
-            </form>
-          </div>
         
         )
     }
