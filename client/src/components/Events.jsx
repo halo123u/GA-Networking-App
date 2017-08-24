7df7639586b87a97927749955c837c35310579e8
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-24 16:47:36 -0400

    put if statements around venue info so it only renders if listed

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -31,8 +31,8 @@ class Events extends Component {
                         <li className='event'>
                             <h2>Event Name:</h2> <h4>{event.name}</h4>
                             <h2>Event Group Name:</h2> <h4>{event.group.name}</h4>
-                            {/* <h2>{event.venue.name}</h2>
-                            <h4>{event.venue.address_1}</h4> */}
+                            {event.venue !== undefined ? <div className='venue-name'><h2>Location:</h2> <h4>{event.venue.name}</h4></div> : null}
+                            {event.venue !== undefined ? <div className='venue-address'><h2>Address:</h2> <h4>{event.venue.address_1}</h4></div> : null}
                             <h2>Event Page:</h2> <p>{event.event_url}</p>
                             <p>Event Description: {event.description}</p>
                         </li>
