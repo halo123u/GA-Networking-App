32582ae0b2f794041a3341af9ace0c19b094c782
Amidou Kante <kamdiou@gmail.com>
2017-08-24 10:55:18 -0400

    css for home and header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -14,7 +14,35 @@ const Header = (props) => {
     
     return(
         <header>
-            <nav>
+            <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+                <header className="mdl-layout__header">
+                    <div className="mdl-layout__header-row">
+                    {/* <!-- Title --> */}
+                    <span className="mdl-layout-title">GA Networking</span>
+                    {/* <!-- Add spacer, to align navigation to the right --> */}
+                    <div className="mdl-layout-spacer"></div>
+                    {/* <!-- Navigation. We hide it in small screens. --> */}
+                    <nav className="mdl-navigation mdl-layout--large-screen-only">
+                        <a className="mdl-navigation__link" href="">Link</a>
+                        <a className="mdl-navigation__link" href="">Link</a>
+                        <a className="mdl-navigation__link" href="">Link</a>
+                        <a className="mdl-navigation__link" href="">Link</a>
+                    </nav>
+                    </div>
+                </header>
+                <div className="mdl-layout__drawer">
+                    <span className="mdl-layout-title">Title</span>
+                    <nav className="mdl-navigation">
+                    <a className="mdl-navigation__link" href="">Link</a>
+                    <a className="mdl-navigation__link" href="">Link</a>
+                    <a className="mdl-navigation__link" href="">Link</a>
+                    <a className="mdl-navigation__link" href="">Link</a>
+                    </nav>
+                </div>
+                <main className="mdl-layout__content">
+                    <div className="page-content"></div>
+                </main>
+            </div>
                 <li style={props.authState ? styles.hideNavItem : styles.showNavItem}><Link to='/'>Home</Link></li>
                 <li style={props.authState ? styles.hideNavItem : styles.showNavItem}><Link to='/login'>Login</Link></li>
                 <li style={!props.authState ? styles.hideNavItem : styles.showNavItem} onClick={props.logOut}><a href="#">Log Out</a></li>
@@ -24,7 +52,6 @@ const Header = (props) => {
                 <li style={props.authState ? styles.hideNavItem : styles.hideNavItem}><Link to='/profileForm'>Profile Form</Link></li>
                 <li style={!props.authState ? styles.hideNavItem : styles.showNavItem}><Link to='/profile/edit'> Edit</Link></li>
                 <li style={!props.authState ? styles.hideNavItem : styles.showNavItem}><Link to='/inbox'>DMs</Link></li>
-            </nav>
         </header>
     )
 }
