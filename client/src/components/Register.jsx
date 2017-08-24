7aaabd2a0a5f594178f62177ed2fd4a3e15df80d
Karen Baque <k.baque@aol.com>
2017-08-24 11:57:45 -0400

    add style to register page

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -43,23 +43,24 @@ class Register extends Component{
             <div className= "register">
                 {redirect ?(<Redirect to='/profile'/>) : null}
                 <form onSubmit={(e) => this.props.submit(e, this.state.username, this.state.password, this.state.email, this.state.firstName, this.state.lastName)}>
-                 <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                 <div class="mdl-textfield mdl-js-textfield">
+                  <input className="mdl-textfield__input" id="name" type="First Name" required="true" name="firstName" placeholder="First Name" value={this.state.firstName} onChange={this.handleInputChange} />
+                  <label className="mdl-textfield__label" for="name">First Name...</label>
+                  <input className="mdl-textfield__input" id="last" type="Last Name" required="true" name="lastName" placeholder="Last Name" value={this.state.lastName} onChange={this.handleInputChange} />
+                  <label className="mdl-textfield__label" for="last">Last Name...</label>
+                  <input className="mdl-textfield__input" id="email" type="email" required="true" name="email" placeholder="Email" value={this.state.email} onChange={this.handleInputChange} />
+                  <label className="mdl-textfield__label" for="email">Email...</label>
                   <input className="mdl-textfield__input" id="user-reg" type="text" required="true" name="username" placeholder="username" value={this.state.username} onChange={this.handleInputChange} />
-                  <label class="mdl-textfield__label" for="user-reg">Username..</label>
-                  <input type="password" required="true" name="password" placeholder="password" value={this.state.password} onChange={this.handleInputChange} />
-                  <input type="email" required="true" name="email" placeholder="email" value={this.state.email} onChange={this.handleInputChange} />
-                  <input type="First Name" required="true" name="firstName" placeholder="First Name" value={this.state.firstName} onChange={this.handleInputChange} />
-                  <input type="Last Name" required="true" name="lastName" placeholder="Last Name" value={this.state.lastName} onChange={this.handleInputChange} />
-                  <input type="submit" value="Register" onClick={this.handleSubmit} />
+                  <label className="mdl-textfield__label" for="user-reg">Username..</label>
+                  <input className="mdl-textfield__input" id="password-reg" type="password" required="true" name="password" placeholder="password" value={this.state.password} onChange={this.handleInputChange} />
+                  <label className="mdl-textfield__label" for="password-reg">Password...</label>
+                  <input className="submit" id="submit" type="submit" value="Register!" onClick={this.handleSubmit} />
+                  {/* <label className="mdl-textfield__label" for="submit">Submit...</label> */}
                 </div>
                 </form>
                 </div>
 
-
-            //  <form action="#">
-            //  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-            // <input class="mdl-textfield__input" id="sample3">
-            // <label class="mdl-textfield__label" for="sample3">Text...</label>
+             
   
 
 
