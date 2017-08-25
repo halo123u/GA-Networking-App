93f44311d9e7cbd43cacb3efe585afcacb86a019
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-08-25 16:31:43 -0400

    add component for rendering saved events

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link, Redirect} from 'react-router-dom';
 import axios from 'axios';
 
 class Events extends Component {
@@ -7,6 +8,7 @@ class Events extends Component {
         this.state = {
             eventInfo: null,
             eventInfoLoaded: false,
+            redirect: false
         }
     }
     componentDidMount() {
@@ -19,18 +21,30 @@ class Events extends Component {
             })
         })
     }
+    addToList=(event)=>{
+        console.log('this link works');
+        console.log(event);
+        // console.log(this.props);
+        axios.post('/events',{event: event , user_id: this.props.data.id});
+        this.setState({
+            redirect:true
+        });
+    }
 
 
     render() {
-      if(this.state.eventInfoLoaded) {  
+      if(this.state.eventInfoLoaded) {
+          const {redirect}= this.state;  
         return (
-            <div className='events-container'>
+            <div className='events-container'> 
+                {redirect ? <Redirect to='/myeventlist'/> : null}
                 <ul className='event-info'>
                   {this.state.eventInfo.map((event, i) => {
                     
                     let eventTime = new Date(event.time).toString().split(' ').slice(0, 5).join(' ');
                       
                     return (
+<<<<<<< HEAD
                         <li className='event' key={i}>
                             <div className="events-page mdl-card mdl-shadow--8dp">
                                 <div className="mdl-card__title">
@@ -45,6 +59,16 @@ class Events extends Component {
                                     <a className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--primary" target="_blank" href = {event.event_url}>Get More Info...</a>
                                 </div>
                             </div>                 
+=======
+                        <li className='event'>
+                            <h2>Event Name:</h2><h4>{event.name}</h4>
+                            <h2>Event Group Name:</h2> <h4>{event.group.name}</h4>
+                            {event.venue !== undefined ? <div className='venue-name'><h2>Location:</h2> <h4>{event.venue.name}</h4></div> : null}
+                            {event.venue !== undefined ? <div className='venue-address'><h2>Address:</h2> <h4>{event.venue.address_1}</h4></div> : null}
+                            <h2>Event Page:</h2> <p>{event.event_url}</p>
+                             ({event.description})
+                             <input type="button" value="add to my list" onClick={()=>this.addToList(event)}/> 
+>>>>>>> add component for rendering saved events
                         </li>
                     )
                   })}  
