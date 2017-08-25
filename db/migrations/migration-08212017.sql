56702e74355f550270b5cd12c744e55b7479b8ac
Amidou Kante <kamdiou@gmail.com>
2017-08-25 15:35:51 -0400

    changes to migaration

--- db/migrations/migration-08212017.sql
+++ db/migrations/migration-08212017.sql
@@ -37,17 +37,17 @@ INSERT INTO users
 VALUES
 ('halo4', '$2a$10$9a00F2FMjMN85t2toteFzu3grrR1aKpF6sEv3O1tIVyEcuKw5qrC.', 'drew@ga.com','Drew', 'Mahrt👾'),
 ('halo1', 'halo1', 'ari@ga.com','Ari', 'Brenner💩'),
-('halo2', 'halo2', 'something2','John', 'Bell'),
+('halo2', 'halo2', 'john@ga.com','John', 'Bell'),
 ('halo3', 'halo3', 'yungT@ga.com','Yung', 'Terminal'),
-('halo5', 'halo5', 'something5','Document', 'Object Model');
+('halo5', 'halo5', 'dom@ga.com','Document', 'Object Model');
 
 INSERT INTO profile
 (age, class, cohort,  interest, location, bio, picture_url, user_id)
 VALUES
 (28, 'WDI', 'Delorean', 'Back-end development', 'New York City', 'I only sleep-walk. When you see me walking, I am sleep-walking. Also Drew, Drew, Drew-Drew. Drewdrew, Drewdrew, Drewdrew. Drew','http://i.imgur.com/TZIScO0.jpg', 1),
 (75, 'WDI', 'twin peaks', 'Front End', 'New York City', 'I like 💩 and only KaHoots','https://i.imgur.com/v3fJR0k.jpg', 2),
-(30, 'IOS Dev','some name', 'Games', 'New York City', 'Want to make a sidescroll shooter game. Need UX designer to help figure out layout of elements on the screen.','https://randomuser.me/api/portraits/women/12.jpg', 3),
-(23, 'IOS Dev','Swoolers', 'DIV Stacking', 'New York City', 'Makign music and swoolin in the next life.','https://ca.slack-edge.com/T0351JZQ0-U5ZF852R0-8463db85a99b-512', 4),
+(30, 'IOS Dev','some name', 'Games', 'New York City', 'Want to make a sidescroll shooter game. Need UX designer to help figure out layout of elements on the screen.','https://ga-core.s3.amazonaws.com/production/uploads/instructor/image/7502/thumb_John_Bell.jpg', 3),
+(23, 'IOS Dev','Swoolers', 'DIV Stacking', 'New York City', 'Making music and swoolin in the next life.','https://ca.slack-edge.com/T0351JZQ0-U5ZF852R0-8463db85a99b-512', 4),
 (23, 'WDI','Delorean', 'Full-Stack', 'New York City', 'My legal full name is "Document Object Model','http://i.imgur.com/hq2ZDOQ.jpg', 5),
 (23, 'IOS Dev','Yung Terminal', 'Games', 'New York City', 'Swoolin in the next life.','https://ca.slack-edge.com/T0351JZQ0-U5ZF852R0-8463db85a99b-512', 6),
 (23, 'IOS Dev','Yung Terminal', 'Games', 'New York City', 'Swoolin in the next life.','https://ca.slack-edge.com/T0351JZQ0-U5ZF852R0-8463db85a99b-512', 7);
