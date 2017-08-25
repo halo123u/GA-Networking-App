533e78442f2b8387b6613b8c5fb4e8f1e9cf03d6
Amidou Kante <kamdiou@gmail.com>
2017-08-25 15:45:51 -0400

    changes to migaration

--- db/migrations/migration-08212017.sql
+++ db/migrations/migration-08212017.sql
@@ -39,22 +39,23 @@ VALUES
 ('halo1', 'halo1', 'ari@ga.com','Ari', 'Brenner💩'),
 ('halo2', 'halo2', 'john@ga.com','John', 'Bell'),
 ('halo3', 'halo3', 'yungT@ga.com','Yung', 'Terminal'),
-('halo5', 'halo5', 'dom@ga.com','Document', 'Object Model');
+('halo5', 'halo5', 'dom@ga.com','Document', 'Object Model'),
+('halo5', 'halo5', 'j@ga.com','J', 'Silverstein 🐈');
+('halo5', 'halo5', 'j@ga.com','Ramsey', 'Nasser 🐈');
 
 INSERT INTO profile
 (age, class, cohort,  interest, location, bio, picture_url, user_id)
 VALUES
 (28, 'WDI', 'Delorean', 'Back-end development', 'New York City', 'I only sleep-walk. When you see me walking, I am sleep-walking. Also Drew, Drew, Drew-Drew. Drewdrew, Drewdrew, Drewdrew. Drew','http://i.imgur.com/TZIScO0.jpg', 1),
 (75, 'WDI', 'twin peaks', 'Front End', 'New York City', 'I like 💩 and only KaHoots','https://i.imgur.com/v3fJR0k.jpg', 2),
-(30, 'IOS Dev','some name', 'Games', 'New York City', 'Want to make a sidescroll shooter game. Need UX designer to help figure out layout of elements on the screen.','https://ga-core.s3.amazonaws.com/production/uploads/instructor/image/7502/thumb_John_Bell.jpg', 3),
+(30, 'IOS Dev','some name', 'Games', 'New York City', 'I know where all the legos are. But they dont know where I am 👀'),'https://ga-core.s3.amazonaws.com/production/uploads/instructor/image/7502/thumb_John_Bell.jpg', 3),
 (23, 'IOS Dev','Swoolers', 'DIV Stacking', 'New York City', 'Making music and swoolin in the next life.','https://ca.slack-edge.com/T0351JZQ0-U5ZF852R0-8463db85a99b-512', 4),
 (23, 'WDI','Delorean', 'Full-Stack', 'New York City', 'My legal full name is "Document Object Model','http://i.imgur.com/hq2ZDOQ.jpg', 5),
-(23, 'IOS Dev','Yung Terminal', 'Games', 'New York City', 'Swoolin in the next life.','https://ca.slack-edge.com/T0351JZQ0-U5ZF852R0-8463db85a99b-512', 6),
-(23, 'IOS Dev','Yung Terminal', 'Games', 'New York City', 'Swoolin in the next life.','https://ca.slack-edge.com/T0351JZQ0-U5ZF852R0-8463db85a99b-512', 7);
+(24, 'IOS Dev','Delorean', 'Front End', 'New York City', 'I own a cat 🐈 named moose  and a moose named cat. ','https://ca.slack-edge.com/T0351JZQ0-U5ZF852R0-8463db85a99b-512', 6),
+(26, 'WDI','Delorean', 'Games', 'New York City', 'I am a NPM package with only one dependency: Ramsey','https://vcard.newschool.edu/?c=500812', 7);
  
 INSERT INTO messages
 (sender_id, recipient_id, content, time_stamp)
 VALUES
-(1,2,'Hey, I  think your projects look cool. Let me know if you would like to collaborate.','10:00am'),
-(2,1,'When do you think you would be free to meet up? I have a meeting at 2 but Im free after.','5:00pm'),
-(3,1,'When do youdlkfmgvklmdfklgmkldfg think you would be I have a meeting at 2 but Im free after.','7:00pm');
+(1,2,'Hi would you like to play hide and seek.','10:00am'),
+(2,1,'No thank you.','5:00pm');
