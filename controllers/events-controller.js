b04886d6bdc4a4df7468066bc0c8321cc54ff208
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-25 10:33:21 -0400

    assign api key variable so its not actually shown in file

--- controllers/events-controller.js
+++ controllers/events-controller.js
@@ -2,13 +2,6 @@ const axios = require('axios');
 
 const eventsController = {
     getAllEvents: (req, res) => {
-        // console.log('this is the controller');
-        // axios.get(`https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=10010&country=United States&topic=technology&city=New York&state=NY&radius=2&page=15&key=2a71648405c6a89156b301b4b4224`)
-        // .then(data => {
-        //    eventData = data;
-        //    return eventData;
-        // }).catch(err => {console.log(err);
-        // })
         console.log("eventsController: res.locals.data", res.locals.data);
         res.json(res.locals.data);
     }
--- services/events-helpers.js
+++ services/events-helpers.js
@@ -1,8 +1,11 @@
 require('isomorphic-fetch');
+require('dotenv').config();
+
+const API_KEY = process.env.API_KEY;
 
 function getEvents (req, res, next) {
     console.log('this is the helper');
-    fetch(`https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=10010&country=United States&topic=technology&city=New York&state=NY&radius=2&page=15&key=2a71648405c6a89156b301b4b4224`)
+    fetch(`https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=10010&country=United States&topic=technology&city=New York&state=NY&radius=2&page=15&key=${API_KEY}`)
         .then(res => {
             return res.json();
         }).then(jsonRes => {
