45864aba8d1c5dd2374662ee0d852a343af19e73
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-08-28 00:04:20 -0400

    remove unused functions

--- services/auth/auth-helpers.js
+++ services/auth/auth-helpers.js
@@ -5,20 +5,6 @@ function comparePass(userPassword, databasePassword) {
     return bcrypt.compareSync(userPassword, databasePassword);
 }
 
-function loginRedirect (req, res, next) {
-    if (req.user) return res.redirect('/');
-    return next();
-}
-
-function loginRequired(req, res, next) {
-    if (req.user === undefined) {
-        return res.redirect('/auth/login');
-    }
-    return next();
-}
-
 module.exports = {
     comparePass,
-    loginRedirect,
-    loginRequired,
 }
