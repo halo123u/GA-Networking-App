39ebfc392cf44e6dccccd3e78fe336818e7d0902
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-28 09:10:59 -0400

    comment event routes

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -1,8 +1,10 @@
+//dependencies and files required
 const express = require('express');
 const events = express.Router();
 const eventsController = require('../controllers/events-controller');
 const eventsHelpers = require('../services/events-helpers');
 
+//routes for the My Events page
 events.get('/', eventsHelpers.getEvents,eventsController.getAllEvents);
 events.get('/myevents/:id',eventsController.getMyEvents);
 events.delete('/:id',eventsController.deleteMyEvents);
