4abe808c7d615aea9008dd98112d8cd0eb2ba281
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-28 09:06:26 -0400

    comment app js file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Express dependencies
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
@@ -6,8 +7,10 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
+//Secret key
 require('dotenv').config();
 
+//Middleware
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -24,17 +27,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-
+//Set up port for Localhost
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`);
 })
 
+//Set up path for yarn build of the app
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+//routes
 const authRoutes = require('./routes/auth-routes');
 app.use('/auth', authRoutes);
 
@@ -47,6 +52,7 @@ app.use('/messages', messageRoutes);
 const eventRoutes = require('./routes/event-routes');
 app.use('/events', eventRoutes);
 
+//error handler
 app.get('*',(req,res)=>{
     res.status(400).json({
         message: 'Not found!',
