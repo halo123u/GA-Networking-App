53dccd2cd532a116a52cff1135e2e39d1ed6385d
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-28 09:10:12 -0400

    comment auth routes file

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,17 +1,21 @@
+//dependencies and required files for auth
 const express = require('express');
 const authRouter = express.Router();
 const passport = require('../services/auth/local');
 const authHelpers = require('../services/auth/auth-helpers');
 const usersController = require('../controllers/users-controller');
 
+//route for registration
 authRouter.post('/register', usersController.create);
 
+//route for login
 authRouter.post('/login', passport.authenticate('local', {
     successRedirect: '/auth/success',
     failureRedirect: '/auth/failure',
     failureFlash: true,
 }))
 
+//routes for success and failure of logging in
 authRouter.get('/success', (req, res) => {
     res.json({
        auth: true,
