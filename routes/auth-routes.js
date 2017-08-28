14023f0f72af069eb27501a029126d1747342cae
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-08-28 00:06:08 -0400

    format code

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -10,30 +10,30 @@ authRouter.post('/login', passport.authenticate('local', {
     successRedirect: '/auth/success',
     failureRedirect: '/auth/failure',
     failureFlash: true,
-}))
+}));
 
 authRouter.get('/success', (req, res) => {
     res.json({
        auth: true,
        message: 'ok',
        user: req.user, 
-    })
-})
+    });
+});
 
 authRouter.get('/failure', (req, res) => {
     res.json({
         auth: false,
         message: 'Login Failed',
         user: null,
-    })
-})
+    });
+});
 
 authRouter.get('/logout', (req, res) => {
     req.logout();
     res.json({
         message: 'Logged Out',
         auth: false,
-    })
-})
+    });
+});
 
 module.exports = authRouter;
