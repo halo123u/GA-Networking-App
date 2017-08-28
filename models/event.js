8034fb527eb35d8fca0493fe6e91ed0623c00515
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-28 09:20:51 -0400

    comment event model

--- models/event.js
+++ models/event.js
@@ -1,17 +1,20 @@
 const db = require('../db/config');
 
 const MyEvent = {
+    //model to add event to personal events page
     addEvent: (event,id)=>{
         return db.one(`
             INSERT INTO events(user_id, data)
             VALUES($1, $2)
             RETURNING *`,[id,event])
     },
+    //model to show events added to personal events page
     getMyEvents: (id)=>{
         return db.query(`
         SELECT * from events where user_id=$1`,[id]);
 
     },
+    //model for deleting event from personal events page
     deleteEvent: (id)=>{
         return db.one(`
         DELETE from events where event_id = $1
