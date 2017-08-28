68faef231dfb3c955974f8428d464a20b3c54e22
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-28 09:14:25 -0400

    comment profile routes file

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,7 +1,9 @@
+//dependencies and required files
 const express = require ('express');
 const profile = express.Router();
 const profileController = require('../controllers/profile-controller');
 
+//routes to individual profiles as well as the profile feed
 profile.get('/feed/',profileController.getAllProfiles);
 profile.get('/:id',profileController.getProfile);
 profile.post('/',profileController.createProfile);
